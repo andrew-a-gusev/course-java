f1d99d661a23ed48cbcd3c88d9db2d63a7940653
andrew-a-gusev <andrew.a.gusev@gmail.com>
2016-09-23 13:17:28 +0300

    Update calculator.java
    Code Review #1 (AG)

--- calc/src/calculator.java
+++ calc/src/calculator.java
@@ -16,7 +16,18 @@ public class calculator {
         String snumb1 = "";
         String snumb2 = "";
         boolean fl = true;
-
+        
+        /**
+         * 1. It is always better to re-use existing fuctions if their interface matches your needs 
+         *    (in this casle String.indexOf, String.substring or String.split)
+         *    If any any moment of time performance of existing function
+         * 2. Standard best practice dictates us to hide such functional blocks inside a function
+         *    For example:
+         *    snumb1 = getFirst(base);
+         *      OR
+         *    Tuple<String,String> operands = getOperands(base);
+         * 
+         */
         for (char c : base.toCharArray()) {
             if (Character.isDigit(c)) {
                 if(fl)snumb1 += c;
@@ -32,7 +43,10 @@ public class calculator {
 
         double numb1 = Double.parseDouble(snumb1);
         double numb2 = Double.parseDouble(snumb2);
-
+        /**
+         * Please think of how to minimize changes when extending the calculator with new functions (say x^y).
+         * What are the objects ? What are the relationships between them ? How to comply to Open/Close principle ?
+         */
         switch (sign){
             case "*": {
                 System.out.print("=" + (numb1*numb2));
